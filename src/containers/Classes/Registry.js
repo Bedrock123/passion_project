a2dee931a7b024434433c76019f074458d7e3e82
Bedrock123 <info@builtbykingwilly.com>
2018-08-09 19:55:23 -0400

    Added in search components

--- src/containers/Classes/Registry.js
+++ src/containers/Classes/Registry.js
@@ -1,28 +1,30 @@
 import React, { Component } from "react";
 import BabsonClasses from "../../components/navigation/classes.json";
+import { FiSearch } from "react-icons/fi";
 
 class Registry extends Component {
-
   renderAllBabsonClasses() {
     return BabsonClasses.map(BabsonClass => {
-        return (
+      return (
         <div className="registry-item">
-            <p>{BabsonClass.class_name}</p>
-            <p>{BabsonClass.day_of_week}</p>
-            <p>{BabsonClass.spots_filled_string}</p>
-            <p>{BabsonClass.course_code}</p>
-            <p>{BabsonClass["professor(s)"]}</p>
+          <p>{BabsonClass.class_name}</p>
+          <p>{BabsonClass.day_of_week}</p>
+          <p>{BabsonClass.spots_filled_string}</p>
+          <p>{BabsonClass.course_code}</p>
+          <p>{BabsonClass["professor(s)"]}</p>
         </div>
-        );
+      );
     });
   }
   render() {
     return (
-        <div className="registry-wrapper">
-            <h2>Registry</h2>
-           
-        </div>
-    )
+      <div className="registry-wrapper">
+        <h2>Registry</h2>
+        <h3 className="page-subheader">
+          Search {' '}<FiSearch />
+        </h3>
+      </div>
+    );
   }
 }
 
