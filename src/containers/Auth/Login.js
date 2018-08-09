a3d17312e122ba91015c3b0294ce7ac1ab5537e1
Bedrock123 <info@builtbykingwilly.com>
2018-08-09 12:01:36 -0400

    Added in basci styleing

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -7,8 +7,20 @@ class Login extends Component {
     return (
       <div>
         <img src={Logo} alt="Intellgent Class Logo" />
+        <h1>Log In</h1>
+        <h2>Log In</h2>
+        <h3>Log In</h3>
+        <h4>Log In</h4>
+        <h5>Log In</h5>
+        <h6>Log In</h6>
+        <p>
+          Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          Nulla vitae elit libero, a pharetra augue. Cras mattis consectetur
+          purus sit amet fermentum. Sed posuere consectetur est at lobortis. Sed
+          posuere consectetur est at lobortis.
+        </p>
         <Button size={"large"} type="primary">
-          Button
+          Log In
         </Button>
       </div>
     );
