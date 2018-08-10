7bb6354e1832a1187041d2388002dc6e37d89386
Bedrock123 <info@builtbykingwilly.com>
2018-08-09 20:16:13 -0400

    Added in search menu;

--- src/containers/Classes/Registry.js
+++ src/containers/Classes/Registry.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import BabsonClasses from "../../components/navigation/classes.json";
 import { FiSearch } from "react-icons/fi";
+import Dock from "react-dock";
 
 class Registry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   renderAllBabsonClasses() {
     return BabsonClasses.map(BabsonClass => {
       return (
@@ -19,9 +24,36 @@ class Registry extends Component {
   render() {
     return (
       <div className="registry-wrapper">
+        <Dock
+          position="top"
+          fluid={false}
+          dockStyle={{
+            background: "#fff"
+          }}
+          size={1000}
+          dimMode={"opaque"}
+          dimStyle={{
+            background: "rgba(0, 0, 0, 0.45)"
+          }}
+          durat
+          isVisible={this.state.isVisible}
+        >
+          <div
+            onClick={() => this.setState({ isVisible: !this.state.isVisible })}
+          >
+            X
+          </div>
+        </Dock>
         <h2>Registry</h2>
-        <h3 className="page-subheader">
-          Search {' '}<FiSearch />
+        <h3
+          className="page-subheader"
+          onClick={() =>
+            this.setState({
+              isVisible: !this.state.isVisible
+            })
+          }
+        >
+          Search <FiSearch />
         </h3>
       </div>
     );
