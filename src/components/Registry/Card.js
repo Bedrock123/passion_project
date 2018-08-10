94559e09201aa70ae4f63a5f3701c36b033b9738
Bedrock123 <info@builtbykingwilly.com>
2018-08-10 19:12:27 -0400

    Added in sdf

--- src/components/Registry/Card.js
+++ src/components/Registry/Card.js
@@ -3,35 +3,36 @@ import React, { Component } from "react";
 class Card extends Component {
   render() {
     return (
-        <div
-        className={"registry-item " + this.props.color }
-      >
+      <div className={"registry-item " + this.props.color}>
         <div className="registry-item-header">
           <p className="main-value">{this.props.BabsonClass.class_name}</p>
         </div>
         <div className="registry-item-body">
-
-          <div className="registry-item-block">
-            <p className="main-value">
+          <div className="registry-item-block" style={{ flex: 2 }}>
             <p className="label">Time</p>
-              {this.props.BabsonClass.day_of_week} | {this.props.BabsonClass.time}
+            <p className="main-value">
+              {this.props.BabsonClass.day_of_week} |{" "}
+              {this.props.BabsonClass.time}
             </p>
           </div>
           <div className="registry-item-block">
-            <p className="main-value">
             <p className="label">Semester</p>
-              {this.props.BabsonClass.semester}
-            </p>
+            <p className="main-value">{this.props.BabsonClass.semester}</p>
           </div>
         </div>
         <div className="registry-item-body">
-        <div className="registry-item-block">
-        <p className="label">Code</p>
-            <p className="main-value">{this.props.BabsonClass.course_code}-{this.props.BabsonClass.course_section}</p>
+          <div className="registry-item-block">
+            <p className="label">Code</p>
+            <p className="main-value">
+              {this.props.BabsonClass.course_code}-
+              {this.props.BabsonClass.course_section}
+            </p>
           </div>
           <div className="registry-item-block">
-          <p className="label">Spots Left</p>
-            <p className="main-value">{this.props.BabsonClass.spots_filled_string}</p>
+            <p className="label">Spots Left</p>
+            <p className="main-value">
+              {this.props.BabsonClass.spots_filled_string}
+            </p>
           </div>
           <div className="registry-item-block">
             <p className="label">Credits</p>
@@ -40,18 +41,19 @@ class Card extends Component {
         </div>
         <div className="registry-item-body">
           <div className="registry-item-block">
-          <p className="label">Professors</p>
-            <p className="main-value">{this.props.BabsonClass["professor(s)"]}</p>
+            <p className="label">Professors</p>
+            <p className="main-value">
+              {this.props.BabsonClass["professor(s)"]}
+            </p>
           </div>
         </div>
-        <div class="registry-badge">
-        {this.props.BabsonClass.spots_taken >= this.props.BabsonClass.spots_available ?
+        <div className="registry-badge">
+          {this.props.BabsonClass.spots_taken >=
+          this.props.BabsonClass.spots_available ? (
             <p>FULL</p>
-            :
-            
+          ) : (
             <p>OPEN</p>
-
-        }
+          )}
         </div>
       </div>
     );
